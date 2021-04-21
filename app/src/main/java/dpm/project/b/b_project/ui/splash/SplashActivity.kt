aeef169f710f908a34b26bf8b4d829cb6c14a735
jjy <skwntnftkgk@gmail.com>
2021-04-21 17:50:30 +0900

    Splash화면 작업

---
+++ app/src/main/java/dpm/project/b/b_project/ui/splash/SplashActivity.kt
@@ -0,0 +1,65 @@
+package dpm.project.b.b_project.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.annotation.Nullable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import dpm.project.b.b_project.MainActivity
+import dpm.project.b.b_project.R
+import dpm.project.b.b_project.R.drawable
+import dpm.project.b.b_project.base.BaseActivity
+import dpm.project.b.b_project.databinding.ActivitySplashBinding
+import dpm.project.b.b_project.dummy.DummyViewModel
+
+class SplashActivity : BaseActivity<ActivitySplashBinding, DummyViewModel>() {
+  override val layoutId: Int = R.layout.activity_splash
+  override val viewModel: DummyViewModel by viewModels()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    Glide.with(this)
+      .asGif()
+      .load(drawable.blife)
+      .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.RESOURCE))
+      .listener(object : RequestListener<GifDrawable> {
+        override fun onLoadFailed(
+          @Nullable e: GlideException?,
+          model: Any,
+          target: Target<GifDrawable>,
+          isFirstResource: Boolean
+        ): Boolean {
+          return false
+        }
+
+        override fun onResourceReady(
+          resource: GifDrawable,
+          model: Any,
+          target: Target<GifDrawable>,
+          dataSource: com.bumptech.glide.load.DataSource,
+          isFirstResource: Boolean
+        ): Boolean {
+          resource.setLoopCount(1)
+          Thread {
+            while (true) {
+              try {
+                Thread.sleep(200)
+              } catch (e: InterruptedException) {
+                e.printStackTrace()
+              }
+              if (!resource.isRunning) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+              }
+            }
+          }.start()
+          return false
+        }
+      }).into(dataBinding.imgSplash)
+  }
+}
